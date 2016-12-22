ab92fdc36b8fdde9f8ac2b443d5284602aa87ede
jamesbyrne89 <mail@jamestbyrne.com>
2016-12-22 13:53:16 +0000

    First commit with questions and click functionality

---
+++ js/quiz.js
@@ -0,0 +1,126 @@
+$(document).ready(function(){
+
+var i = 0;
+
+
+// Declare list of questions
+
+const questions = new Array(
+
+question1 = {
+question: "Who is the patron saint of Spain?",
+answerOne: "Saint James",
+answerTwo: "Saint John",
+answerThree: "Saint Benedict",
+answerFour: "Saint Peter"
+},
+question2 = {
+question: "Which king was married to Eleanor of Aquitaine?",
+answerOne: "Henry I",
+answerTwo: "Henry V",
+answerThree: "Henry II",
+answerFour: "Richard I"
+},
+question3 = {
+question: "The Newlyn School of the late 19th century, is associated with which group of people?",
+answerOne: "Method actors",
+answerTwo: "Circus entertainers",
+answerThree: "Painters",
+answerFour: "Musicians"
+},
+question4 = {
+question: "If you planted the seeds of Quercus robur, what would grow?",
+answerOne: "Trees",
+answerTwo: "Flowers",
+answerThree: "Vegetables",
+answerFour: "Grain"
+},
+question5 = {
+question: "Which of these African countries is situated south of the equator?",
+answerOne: "Ethiopia",
+answerTwo: "Nigeria",
+answerThree: "Zambia",
+answerFour: "Chad"
+},
+question6 = {
+question: "Which scientific unit is named after an Italian nobleman?",
+answerOne: "Pascal",
+answerTwo: "Ohm",
+answerThree: "Volt",
+answerFour: "Hertz"
+},
+
+question7 = {
+question: "Which of these creatures are most associated with the naturalist and artist John James Audubon?",
+answerOne: "Beetles",
+answerTwo: "Butterflies",
+answerThree: "Birds",
+answerFour: "Bats"
+},
+
+question8 = {
+question: "Which of these is not one of the American Triple Crown horse races?",
+answerOne: "Arlington Million",
+answerTwo: "Belmont Stakes",
+answerThree: "Kentucky Derby",
+answerFour: "Preakness Stakes"
+},
+
+question9 = {
+question: "Which is the deepest lake in the world?",
+answerOne: "Lake Baikal",
+answerTwo: "Lake Superior",
+answerThree: "Caspian Sea",
+answerFour: "Lake Victoria"
+},
+
+question10 = {
+question: "Which boxer was famous for striking the gong in the introduction to J. Arthur Rank films?",
+answerOne: "Bombardier Billy Wells",
+answerTwo: "Freddie Mills",
+answerThree: "Terry Spinks",
+answerFour: "Don Cockell"
+}
+);
+
+
+// Default question
+
+        $("#question").text(questions[i].question);
+        $("#answer-one").text(questions[i].answerOne);
+        $("#answer-two").text(questions[i].answerTwo);
+        $("#answer-three").text(questions[i].answerThree);
+        $("#answer-four").text(questions[i].answerFour);
+
+// Grab the next question in array
+
+function nextItem() {
+	if (i < (questions.length - 1)){
+    i += 1; // increase i by one
+    $("#question-number-background").text("Q" + (i+1));
+    console.log(questions.length);
+    return questions[i]; // give us back the item of where we are now
+    }
+else{
+	return;
+}}
+
+
+
+    $('#submit-answer').on('click', (function() {
+
+
+    	nextItem()
+
+        $("#question").text(questions[i].question);
+        $("#answer-one").text(questions[i].answerOne);
+        $("#answer-two").text(questions[i].answerTwo);
+        $("#answer-three").text(questions[i].answerThree);
+        $("#answer-four").text(questions[i].answerFour);
+        $("#answers").removeClass("executed");
+        //$("#answers").toggleClass("after");
+        
+    })
+    );
+});
+
